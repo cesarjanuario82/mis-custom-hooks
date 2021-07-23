47ef7be85b6f9a4c775e2907b9587ca6a0b5687c
Cesar Marin <cesarjanuario@hotmail.com>
2021-07-22 19:11:10 -0500

    Primer Commit

---
+++ useFetch.js
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from 'react'
+
+export const useFetch = (url) => {
+
+    const isMounted = useRef(true);
+   
+    const [state, setstate] = useState( {data:null, loading: true, error: null} );
+
+    useEffect(() => {
+      return () => {
+        isMounted.current = false;
+      }
+    }, [])
+
+    useEffect(() => {
+
+        setstate({// reiniciar el loading cada que entre
+            loading:true,
+            error:null,
+            data:null
+        });
+        
+        fetch( url )
+        .then(
+          resp => resp.json()
+            )
+          .then( data => {
+
+              if ( isMounted.current ) {
+
+                setstate({
+                    loading:false,
+                    error:null,
+                    data
+                });
+
+              } 
+
+            });
+        
+    }, [url]);
+
+    return state;
+
+}
